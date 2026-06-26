45916bb9378ad4ff135921d50cb8b40b925d8ccf
Klydo131 <124277248+Klydo131@users.noreply.github.com>
2026-06-26 15:43:18 +0800

    fix: Vercel production routing

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/api/:path*',
-      },
-    ]
+    if (process.env.NODE_ENV === 'development') {
+      return [
+        {
+          source: '/api/:path*',
+          destination: 'http://127.0.0.1:5000/api/:path*',
+        },
+      ]
+    }
+    return []
   },
 };
 
